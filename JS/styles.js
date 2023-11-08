370d2a62a7f609d10be34c467d9eb21d882edb79
tyhieshajohnson <johnsontyhiesha@gmail.com>
2023-11-08 23:23:48 +0200

    1-2

---
+++ JS/styles.js
@@ -0,0 +1,6 @@
+//Make a variable with a data of a string and check the type of this variable.
+let fullName = "Tyhiesha Julia Johnson";
+//console.log(typeof fullName);
+fullName = 11;
+console.log(typeof fullName);
+//Go ahead and create a variable with every data type you know of. Double check that you covered all of them by using the typeof check.
